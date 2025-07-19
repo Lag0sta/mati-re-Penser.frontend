947167dc0dc5bd1d90a995bd7836cc00114d6868
LRonnet <leo.ronnet@gmail.com>
2025-07-19 21:54:01 +0200

    ajout lockTopic & unlockTopic

--- src/components/TopicThread.tsx
+++ src/components/TopicThread.tsx
@@ -51,11 +51,14 @@ function TopicThread({ setMainComponent, setIsModalOpen, setIsMessageModalOpen, 
         setAuthType("lockTopic");
         setIsModalOpen(true)
         setModalComponent('auth');
+        setIsLocked(true)
     }
 
     const handleUnlockTopic = () => {
-        // auth({token, password, setPassword, setIsModalOpen, setIsMessageModalOpen, setErrorMessage, setSuccessMessage, setModalComponent})
-        // setIsLocked(false)
+        setAuthType("lockTopic");
+        setIsModalOpen(true)
+        setModalComponent('auth');
+        setIsLocked(false)
     }
 
     return (
@@ -139,7 +142,7 @@ function TopicThread({ setMainComponent, setIsModalOpen, setIsMessageModalOpen, 
                                 </div>
                             </div>
                             <div className=" h-full w-full flex justify-end items-center">
-                                {token &&
+                                {(token && !isLocked) &&
                                     <div className="w-fit flex justify-between ">
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 m-1 cursor-pointer">
                                             <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
--- src/utils/topicActions.ts
+++ src/utils/topicActions.ts
@@ -64,7 +64,7 @@ export async function editTopic({ topicData }: topicProps) {
             })
         })
         const editResponse = await editTopic.json()
-
+        console.log("editResponse", editResponse)
         return editResponse
 
     } catch (error) {
